dc90a0a7d79e2c65ca2979985d882964bf7a7596
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2026-07-25 15:57:34 +0300

    fix: use Logform.Format return type for JSR slow-types compliance

--- src/winston.ts
+++ src/winston.ts
@@ -1,4 +1,4 @@
-import { format, type Format } from "winston";
+import { format, type Logform } from "winston";
 import { createPiiMasker, type PiiMaskerOptions } from "./index.js";
 
 export type WinstonPiiMaskingOptions = PiiMaskerOptions;
@@ -17,7 +17,7 @@ export type WinstonPiiMaskingOptions = PiiMaskerOptions;
  * });
  * ```
  */
-export const winstonPiiMasking = (options: WinstonPiiMaskingOptions = {}): Format => {
+export const winstonPiiMasking = (options: WinstonPiiMaskingOptions = {}): Logform.Format => {
   const masker = createPiiMasker(options);
   return format((info) => masker.value(info))();
 };
